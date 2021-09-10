3ac80893b8ef238000e130bccd6a7050b66faff9
satocchi0416sh <satoyoshi44@gmail.com>
2021-09-10 21:27:25 +0900

    the first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="todo-app">
-      <h1>Todo appp</h1>
+      <TodoList />
     </div>
   );
 }
